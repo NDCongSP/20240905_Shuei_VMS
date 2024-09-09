f5867acdc82abadf85e4ec18ca515cb0b538c5d6
NDCongSP <ndcong08cddv02@gmail.com>
2024-09-09 12:43:35 +0700

    up OK

---
+++ info/SQLQuery19.sql
@@ -0,0 +1,26 @@
+ SELECT *
+  FROM AspNetRoles
+  
+  SELECT *
+  FROM AspNetUserRoles
+  
+  SELECT *
+  FROM [AspNetUsers]
+
+  SELECT *
+  FROM [AspNetUsers]
+
+  
+  --SELECT *
+  --FROM RoleToPermissions
+  -- SELECT *
+  --FROM RoleToPermissionsTenant
+
+  --select * from Unit
+  --select * from Category
+  --select * from product
+
+  --truncate table unit
+  --truncate table Category
+  --truncate table product
+  --Xb0zPmUsezyXb26jvz0YQETK87EB5LDFdpYPWI7FQeCtGxnQyqmXr3A7lyh24FXOnLZqDANDUcvhgWmwCGQKjg==
